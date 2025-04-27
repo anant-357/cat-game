018da08e2b1be124094844a26224f87576b10d05
Anant Sharma <anantsh.88@gmail.com>
2025-04-27 23:44:03 +0530

    init Bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use bevy::prelude::*;
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            mode: bevy::window::WindowMode::Fullscreen(
+                MonitorSelection::Primary,
+                VideoModeSelection::Current,
+            ),
+            title: "Minesweeper".to_string(),
+            ..Default::default()
+        }),
+        ..Default::default()
+    }));
+    #[cfg(feature = "debug")]
+    app.add_plugins((
+        EguiPlugin {
+            enable_multipass_for_primary_context: true,
+        },
+        WorldInspectorPlugin::new(),
+    ));
+    app.run();
 }
